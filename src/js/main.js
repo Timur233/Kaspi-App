3e6d16b132b855debaf927dbf71634040e3de66a
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-10-15 13:55:04 +0600

    Поправил стили - Изменил масштаб - Отступы - Кнопки

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import '../scss/main.scss';
+
+// const categoryValidator = (node) => {
+
+// }
+
+// class CategoryValidator {
+//     constructor(node, type) {
+//         this.node = node,
+//         this.type = type;
+//     }
+// }
+
+// const validator = new CategoryValidator(document.createElement('div'), 'default');
