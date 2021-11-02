6c623b7b2b0e39681c5415328be11d3d9c72adf7
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-11-03 01:06:44 +0600

    Product Validator

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    build: {
+        minify:        false,
+        rollupOptions: {
+            output: {
+                assetFileNames: '[name].[ext]',
+            },
+        },
+    },
+};
