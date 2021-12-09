e0aff28cd5e6837a379cb0cec6ab2670a19ec531
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-12-09 20:46:44 +0600

    Orders

---
+++ src/js/order.js
@@ -0,0 +1,26 @@
+import '../scss/main-product.scss';
+
+const config = {
+    ssl:           'https://',
+    host:          'asi-mart.kz',
+    session:       '49a03091-bdf0-4fba-9fa1-230907c5d09d',
+    filter:        '',
+    supplierUuid:  '',
+    query:         '',
+    countProducts: '',
+    page:          1,
+};
+
+const page = async () => {
+    const orderList = () => {
+        console.log(this);
+    };
+
+    const request = () => {
+        console.log(123);
+    };
+
+    orderList();
+};
+
+page();
